70746cfc83231d91ef5af352af76c51c7487942f
VIVEK <99870677+Roninopp@users.noreply.github.com>
2022-12-14 20:26:42 +0530

    Update #config.py

--- #config.py
+++ #config.py
@@ -10,3 +10,7 @@ import Skynet_System.config as Config
     Skynet_logs = "-1001564289796"
     Skynet_approved_logs = "-1001564289796"
     GBAN_MSG_LOGS = "-1001564289796"
+    BOT_TOKEN: str = "5402826348:AAGjSCrB8iAesNZaobMJgRpbK2yX_IhvHt0"
+    MONGO_DB_URL: str = "mongodb+srv://DARKAMAN:DARKAMAN@cluster0.snqhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+     
